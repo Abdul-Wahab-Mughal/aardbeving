290727ed637f8cd7e790a3c9b7e5e640f5f319c2
Abdul-Wahab-Mughal <abdulwahabmughal10@gmail.com>
2026-02-27 23:15:13 +0500

    update halfe question ?

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import herobg from "../assets/hero-bg.jpg";
 import { ChevronRight } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Context() {
+  const [step, setStep] = useState(0);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    traject: "",
+    problem: "",
+    status: [],
+  });
+
+  const nextStep = () => setStep(step + 1);
+  const prevStep = () => setStep(step - 1);
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+      if (checked) {
+        setFormData({
+          ...formData,
+          status: [...formData.status, value],
+        });
+      } else {
+        setFormData({
+          ...formData,
+          status: formData.status.filter((item) => item !== value),
+        });
+      }
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    alert("Form submitted!");
+  };
+
   function scrollup() {
     const section = document.getElementById("contact");
     section.scrollIntoView({
@@ -10,8 +52,9 @@ export default function Context() {
       block: "start",
     });
   }
+
   return (
-    <amin>
+    <main>
       <section className="h-screen w-full p-5 bg-[--background_box]">
         <div className="relative w-full h-full rounded-2xl overflow-hidden">
           <img src={herobg} alt="" className="w-full h-full object-cover" />
@@ -43,17 +86,188 @@ export default function Context() {
       </section>
       {/* contact  */}
       <section id="contact" className=" p-5 bg-[--background_box]">
-        <div className="bg-white p-5 md:p-10 rounded-xl text-black space-y-5">
-          <h2 className="text-[--background_box] text-xl md:text-4xl font-semibold uppercase">
-            Vragen, Opmerkingen of aanvragen voor nieuwe projecten?
-          </h2>
-          <p>
-            Kom in contact met ons en laat je vraag achter via dit
-            contactformulier.
-          </p>
-          <div>Form Contact Coming Soon...</div>
+        <div className="bg-white p-5 md:p-10 rounded-xl text-black">
+          <h2>Step {step}</h2>
+
+          <form onSubmit={handleSubmit}>
+            <div className="space-y-5">
+              <h2 className="text-[--background_box] text-xl md:text-4xl font-semibold uppercase">
+                Vragen, Opmerkingen of aanvragen voor nieuwe projecten?
+              </h2>
+              <p>
+                Kom in contact met ons en laat je vraag achter via dit
+                contactformulier.
+              </p>
+              {/* STEP 0 */}
+              {step === 0 && (
+                <div className="space-y-5">
+                  <div className="flex gap-5">
+                    <Input
+                      required
+                      placeholder="NAAM"
+                      className="border-0 !border-b-2 border-[--background_box]"
+                    />
+                    <Input
+                      placeholder="BEDRIJF (OPTIONEEL)"
+                      className="border-0 !border-b-2 border-[--background_box]"
+                    />
+                  </div>
+                  <div className="flex gap-5">
+                    <Input
+                      required
+                      placeholder="E-MAIL"
+                      type="email"
+                      className="border-0 !border-b-2 border-[--background_box]"
+                    />
+                    <Input
+                      placeholder="TELEFOONNUMER (OPTIONEEL)"
+                      className="border-0 !border-b-2 border-[--background_box]"
+                    />
+                  </div>
+                  <Textarea
+                    placeholder="BERICHT"
+                    row={24}
+                    className="border-0 !border-b-2 border-[--background_box]"
+                  />
+
+                  <p className="space-x-2">
+                    <input
+                      id="id_agreed_to_privacy_policy"
+                      name="agreed_to_privacy_policy"
+                      type="checkbox"
+                      class="checkbox"
+                      required=""
+                    />
+                    <label for="id_agreed_to_privacy_policy">
+                      Ik ga akkoord met de privacyverklaring (lees onze
+                      privacyverklaring)
+                    </label>
+                  </p>
+                </div>
+              )}
+
+              {/* STEP 1 */}
+              {step === 1 && (
+                <>
+                  <h3>1. Met welk traject heeft u te maken?</h3>
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="traject"
+                      value="IMG – Schade"
+                      onChange={handleChange}
+                    />
+                    IMG – Schade
+                  </label>
+                  <br />
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="traject"
+                      value="NCG – Versterking"
+                      onChange={handleChange}
+                    />
+                    NCG – Versterking
+                  </label>
+                  <br />
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="traject"
+                      value="Duurzaam herstel"
+                      onChange={handleChange}
+                    />
+                    Duurzaam herstel
+                  </label>
+                </>
+              )}
+
+              {/* STEP 2 */}
+              {step === 2 && (
+                <>
+                  <h3>2. Waar loopt u vast?</h3>
+
+                  <textarea
+                    name="problem"
+                    placeholder="Beschrijf uw situatie..."
+                    value={formData.problem}
+                    onChange={handleChange}
+                    required
+                    rows="5"
+                    style={{ width: "100%" }}
+                  />
+                </>
+              )}
+
+              {/* STEP 3 */}
+              {step === 3 && (
+                <>
+                  <h3>3. Wat is al gebeurd?</h3>
+
+                  <label>
+                    <input
+                      type="checkbox"
+                      value="Opname of rapport opgesteld"
+                      onChange={handleChange}
+                    />
+                    Er is een opname of rapport opgesteld
+                  </label>
+                  <br />
+
+                  <label>
+                    <input
+                      type="checkbox"
+                      value="Besluit genomen"
+                      onChange={handleChange}
+                    />
+                    Er is een besluit genomen
+                  </label>
+                  <br />
+
+                  <label>
+                    <input
+                      type="checkbox"
+                      value="Bezwaar loopt"
+                      onChange={handleChange}
+                    />
+                    Er loopt bezwaar of procedure
+                  </label>
+                </>
+              )}
+
+              <div className="flex justify-between">
+                <button
+                  type="button"
+                  onClick={prevStep}
+                  className=" uppercase px-5 py-2.5 bg-[--background_box] rounded-full text-white"
+                >
+                  Back
+                </button>
+                <button
+                  type="button"
+                  onClick={nextStep}
+                  className={`${
+                    step === 3 && "hidden"
+                  } uppercase px-5 py-2.5 bg-[--background_box] rounded-full text-white`}
+                >
+                  Next
+                </button>
+                <button
+                  type="submit"
+                  className={`${
+                    step !== 3 && "hidden"
+                  } uppercase px-5 py-2.5 bg-[--background_box] rounded-full text-white`}
+                >
+                  Verstuur aanvraag
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </section>
-    </amin>
+    </main>
   );
 }
